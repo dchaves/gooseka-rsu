5c0650c1f5141217f1e1ae2706c4ea56f45ebdca
David Chaves Dieguez <dchaves@gradiant.org>
2020-04-06 13:46:56 +0200

    Separadas comunicaciones LoRA y WiFi

--- esp32_gateway.ino
+++ esp32_gateway.ino
@@ -1,5 +1,10 @@
-#include <SPI.h>
-#include <LoRa.h>
+#define USE_WIFI
+
+#ifdef USE_WIFI
+    #include "esp32_radio_wifi.h"
+#else
+    #include "esp32_radio_lora.h"
+#endif
 
 #include "esp32_controller_defs.h"
 #include "gooseka_structs.h"
@@ -13,53 +18,6 @@
 #define SOF_2 0xAD
 
 QueueHandle_t control_queue;
-
-void init_radio() {
-    // Set SPI LoRa pins
-    SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_SS);
-
-    // Setup LoRa transceiver module
-    LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);
-    if(!LoRa.begin(LORA_BAND)) {
-        DEBUG_PRINTLN("LoRa initialization error");
-        while(1);
-    }
-
-    LoRa.setCodingRate4(LORA_CODING_RATE);
-    LoRa.setSpreadingFactor(LORA_SPREADING_FACTOR);
-    LoRa.setSignalBandwidth(LORA_BANDWIDTH);
-    LoRa.setTxPower(LORA_TX_POWER);
-    LoRa.setSyncWord(LORA_SYNCWORD);
-}
-
-void send_via_radio(uint8_t* payload, size_t size) {
-    LoRa.beginPacket();
-    LoRa.write(payload, size); 
-    if(LoRa.endPacket() == 1) {
-        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    }
-}
-
-int receive_radio_packet(uint8_t* buffer, int size) {
-    uint8_t index;
-
-    int packetSize = LoRa.parsePacket();
-    if (packetSize == size) {
-        DEBUG_PRINT("INCOMING LORA ");
-        DEBUG_PRINTLN(LoRa.packetRssi());
-        index = 0;
-        while (LoRa.available() && index < size) {
-            buffer[index] = LoRa.read();
-            index++;
-        }
-    }
-
-    return packetSize;
-}
-
-int16_t radio_rssi() {
-    return LoRa.packetRssi();
-}
 
 // CPU #1
 void radio_receive_task(void* param) {
@@ -80,7 +38,7 @@ void radio_receive_task(void* param) {
             // Serial.write(radio_rssi());
         }
 
-        if(millis() - last_sent_millis > LORA_SLOWDOWN) {
+        if(millis() - last_sent_millis > RADIO_SLOWDOWN) {
             // Send enqueued radio msgs
             xQueueReceive(control_queue, &control, 0);
             if(control.magic_number == MAGIC_NUMBER) {
---
+++ esp32_radio_wifi.h
@@ -0,0 +1,76 @@
+#ifndef ESP32_RADIO_WIFI_H
+#define ESP32_RADIO_WIFI_H
+
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <WiFiAP.h>
+
+#include "esp32_controller_defs.h"
+#include "gooseka_structs.h"
+#include "esp32_controller_helpers.h"
+
+static const IPAddress LOCAL_IP(WIFI_GATEWAY_IP);
+static const IPAddress NETMASK(WIFI_NETMASK);
+static const IPAddress MULTICAST_IP(WIFI_MULTICAST_IP);
+WiFiUDP udp;
+
+void WiFiEvent(WiFiEvent_t event){
+    switch(event) {
+        case SYSTEM_EVENT_AP_START:
+            break;
+        case SYSTEM_EVENT_AP_STACONNECTED:
+            DEBUG_PRINTLN("NEW CLIENT CONNECTED");
+            break;
+        case SYSTEM_EVENT_AP_STADISCONNECTED:
+            DEBUG_PRINTLN("CLIENT DISCONNECTED");
+            break;
+        case SYSTEM_EVENT_STA_CONNECTED:
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            break;
+    }
+}
+
+void init_radio() {
+    // Set event handler
+    WiFi.onEvent(WiFiEvent);
+
+    // Set WiFi AP
+    WiFi.softAPConfig(LOCAL_IP, LOCAL_IP, NETMASK);
+    WiFi.softAP(WIFI_BSSID, WIFI_PASSWORD);
+
+    // Disable power saving
+    WiFi.setSleep(false); 
+
+    // Start multicast UDP interface
+    udp.beginMulticast(MULTICAST_IP, WIFI_MULTICAST_PORT);
+}
+
+void send_via_radio(uint8_t* payload, size_t size) {
+    udp.beginMulticastPacket();
+    udp.write(payload, size);
+    if(udp.endPacket() != 1) {
+        DEBUG_PRINTLN("Error sending packet");
+    } else {
+        DEBUG_PRINT("Sent ");
+        DEBUG_PRINT(size);
+        DEBUG_PRINT(" bytes to ");
+        DEBUG_PRINTLN(udp.remoteIP());
+    }
+}
+
+int receive_radio_packet(uint8_t* buffer, int size) {
+    int packetSize = 0;
+    packetSize = udp.parsePacket();
+    if(packetSize == size) {
+        udp.read(buffer, size);
+        DEBUG_PRINTLN("INCOMING WIFI");
+    }
+    return packetSize;
+}
+
+int16_t radio_rssi() {
+    return WiFi.RSSI();
+}
+
+#endif //ESP32_RADIO_WIFI_H
